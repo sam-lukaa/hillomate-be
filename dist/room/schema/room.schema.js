556d10afb476547c990b8d91fb372f6d5a12a195
Lucas <iamsamuelfak@gmail.com>
2025-01-22 09:51:14 +0100

    ci: increase call time - 30mins

--- dist/room/schema/room.schema.js
+++ dist/room/schema/room.schema.js
@@ -32,7 +32,7 @@ __decorate([
 ], Room.prototype, "createdAt", void 0);
 __decorate([
     (0, mongoose_1.Prop)({
-        default: () => new Date(Date.now() + 5 * 60 * 1000),
+        default: () => new Date(Date.now() + 30 * 60 * 1000),
     }),
     __metadata("design:type", Date)
 ], Room.prototype, "expiryTime", void 0);
